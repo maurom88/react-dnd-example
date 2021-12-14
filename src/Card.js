c125a309e09e92df2e9d1e70ebbd763329452a70
Mauro Meden <mauro.meden@gmail.com>
2021-12-14 14:53:53 -0600

    Make cards draggable

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,18 @@
 import React from "react";
+import { useDrag } from "react-dnd";
 
 const cardStyle = {
-    borderStyle: "solid",
-    minWidth: "20%",
-}
+  borderStyle: "solid",
+  minWidth: "20%",
+};
 
 export default function Card({ id, name, content }) {
+  const [collected, drag] = useDrag(() => ({
+    type: "card",
+    item: { id },
+  }));
   return (
-    <div style={cardStyle} key={id}>
+    <div ref={drag} style={cardStyle} key={id}>
       <h5>{name}</h5>
       <p>{content}</p>
     </div>

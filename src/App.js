1a3a47194894357ee2c9f150413a9730029a7a84
Mauro Meden <mauro.meden@gmail.com>
2021-12-13 20:11:04 -0600

    Prepare empty board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { mockDraggableCards } from "./mockData";
+import "./App.css";
+
+export default function App() {
+  const [draggableCards, setDraggableCards] = useState(mockDraggableCards);
+  const [droppableCards, setDroppableCards] = useState([]);
+
   return (
     <div className="App">
-
+      <div>
+        <h2>Draggable cards</h2>
+        <div></div>
+      </div>
+      <div>
+        <h2>Droppable area</h2>
+        <div></div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/mockData.js
@@ -0,0 +1,17 @@
+export const mockDraggableCards = [
+  {
+    id: 0,
+    name: "First card",
+    content: "This is the first card",
+  },
+  {
+    id: 1,
+    name: "Second card",
+    content: "This is the second card",
+  },
+  {
+    id: 2,
+    name: "Third card",
+    content: "This is the third card",
+  },
+];

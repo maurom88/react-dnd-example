d8218fab15f97ead1ccbd14ca9756d38902799e7
Mauro Meden <mauro.meden@gmail.com>
2021-12-14 08:33:25 -0600

    Create Section component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Card from "./Card";
+import Section from "./Section";
 
 import { mockDraggableCards } from "./mockData";
 import "./App.css";
@@ -11,22 +11,8 @@ export default function App() {
 
   return (
     <div className="App">
-      <div className="App-section">
-        <h2 className="App-cards-title">Draggable cards</h2>
-        <div className="App-cards-list">
-          {draggableCards.map((card) => (
-            <Card id={card.id} name={card.name} content={card.content} />
-          ))}
-        </div>
-      </div>
-      <div className="App-section">
-        <h2 className="App-cards-title">Droppable area</h2>
-        <div className="App-cards-list">
-        {droppableCards.map((card) => (
-            <Card id={card.id} name={card.name} content={card.content} />
-          ))}
-        </div>
-      </div>
+      <Section title="Draggable cards" cards={draggableCards} />
+      <Section title="Droppable cards" cards={droppableCards} />
     </div>
   );
 }
---
+++ src/Section.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Card from "./Card";
+
+export default function Section({title, cards}) {
+  return (
+    <div className="App-section">
+      <h2 className="App-cards-title">{title}</h2>
+      <div className="App-cards-list">
+        {cards.map((card) => (
+          <Card id={card.id} name={card.name} content={card.content} />
+        ))}
+      </div>
+    </div>
+  );
+}

88211f7dd842016be8769808d495ea1bd348117a
Mauro Meden <mauro.meden@gmail.com>
2021-12-14 15:29:16 -0600

    Make righthand section droppable

--- src/Section.js
+++ src/Section.js
@@ -1,12 +1,17 @@
 import React from "react";
+import { useDrop } from "react-dnd";
 
 import Card from "./Card";
 
-export default function Section({title, cards}) {
+export default function Section({ title, cards }) {
+  const [collectedProps, drop] = useDrop(() => ({
+    accept: "card",
+  }));
+
   return (
     <div className="App-section">
       <h2 className="App-cards-title">{title}</h2>
-      <div className="App-cards-list">
+      <div ref={drop} className="App-cards-list">
         {cards.map((card) => (
           <Card id={card.id} name={card.name} content={card.content} />
         ))}

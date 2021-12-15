d69dac0c99ec46a9af55cedc8088303bfbff280c
Mauro Meden <mauro.meden@gmail.com>
2021-12-15 09:31:44 -0600

    Remove useDrop from Section

--- src/Section.js
+++ src/Section.js
@@ -1,19 +1,14 @@
 import React from "react";
 import update from 'immutability-helper'
 
-import { useDrop } from "react-dnd";
-
 import Card from "./Card";
 
 export default function Section({ title, cards }) {
-  const [{}, drop] = useDrop(() => ({
-    accept: "card",
-  }));
 
   return (
     <div className="App-section">
       <h2 className="App-cards-title">{title}</h2>
-      <div ref={drop} className="App-cards-list">
+      <div className="App-cards-list">
         {cards.map((card) => (
           <Card id={card.id} name={card.name} content={card.content} />
         ))}

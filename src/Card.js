d5082b13a34cb99c20b9f3a64401f78c567560cc
Mauro Meden <mauro.meden@gmail.com>
2021-12-16 08:22:38 -0600

    Copy drag and drop from rdnd example

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,77 @@
-import React from "react";
-import { useDrag } from "react-dnd";
+import React, { useRef } from "react";
+import { useDrag, useDrop } from "react-dnd";
 
 const cardStyle = {
   borderStyle: "solid",
   minWidth: "20%",
 };
 
-export default function Card({ id, name, content }) {
-  const [{}, drag] = useDrag(() => ({
+export default function Card({ id, name, content, index, moveCard }) {
+  const ref = useRef(null);
+  const [{ handlerId }, drop] = useDrop({
+    accept: "card",
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      };
+    },
+    hover(item, monitor) {
+      if (!ref.current) {
+        return;
+      }
+      const dragIndex = item.index;
+      const hoverIndex = index;
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return;
+      }
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect();
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2;
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset();
+      // Get pixels to the top
+      const hoverClientY = clientOffset.y - hoverBoundingRect.top;
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return;
+      }
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return;
+      }
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex);
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex;
+    },
+  });
+  const [{ isDragging }, drag] = useDrag({
     type: "card",
-    item: { id },
-  }));
+    item: () => {
+      return { id, index };
+    },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+  const opacity = isDragging ? 0 : 1;
+  drag(drop(ref));
   return (
-    <div ref={drag} style={cardStyle} key={id}>
+    <div
+      ref={ref}
+      style={{ ...cardStyle, opacity }}
+      key={id}
+      data-handler-id={handlerId}
+    >
       <h5>{name}</h5>
       <p>{content}</p>
     </div>
--- src/Section.js
+++ src/Section.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 import update from "immutability-helper";
 
 import { mockDraggableCards } from "./mockData";
@@ -7,12 +7,34 @@ import Card from "./Card";
 export default function Section({ title }) {
   const [cards, setCards] = useState(mockDraggableCards);
 
+  const moveCard = useCallback(
+    (dragIndex, hoverIndex) => {
+      const dragCard = cards[dragIndex];
+      setCards(
+        update(cards, {
+          $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragCard],
+          ],
+        })
+      );
+    },
+    [cards]
+  );
+
   return (
     <div className="App-section">
       <h2 className="App-cards-title">{title}</h2>
       <div className="App-cards-list">
-        {cards.map((card) => (
-          <Card id={card.id} name={card.name} content={card.content} />
+        {cards.map((card, index) => (
+          <Card
+            key={card.id}
+            id={card.id}
+            name={card.name}
+            content={card.content}
+            index={index}
+            moveCard={moveCard}
+          />
         ))}
       </div>
     </div>
